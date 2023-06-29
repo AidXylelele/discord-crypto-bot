a8dc8aa90db5ad25a69291d5e912b3c31d596a74
AIDXYLELELE <warboyka48@gmail.com>
2023-06-29 18:39:56 +0200

    fix: Added .env to gitignore

--- src/index.js
+++ src/index.js
@@ -13,27 +13,7 @@ const client = new Discord.Client({
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
 
-  const websites = ['https://example1.com', 'https://example2.com', 'https://example3.com'];
-
-  setInterval(async () => {
-    for (let website of websites) {
-   
-      await page.goto(website);
-      const data = await page.evaluate(() => {
-      
-        return document.body.innerHTML;
-      });
-
-    
-      db.query('INSERT INTO data (site, data) VALUES (?, ?)', [website, data], (err, result) => {
-        if (err) throw err;
-        console.log('Data inserted');
-      });
-    }
-  }, 1000 * 60 * 60); 
 });
 
 client.on("messageCreate", (msg) => {
